f5950f2cfaa451cc337dae69b64fa8661c262d13
Farshid Rezaei <farshid.net1@yahoo.com>
2022-07-20 12:45:33 +0430

    add i18n

---
+++ src/Languages/en/exceptions.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'invalidArgument' => 'Entered Argument is invalid.',
+    'applicationIsNotActive' => 'The application for your request is not active.',
+    'checkoutHasNotWithdrawalState' => 'This checkout is not payable.',
+    'modelNotFound' => '404 Error occurred.',
+    'methodNotAllowed' => 'Method not allowed.',
+    'throttleRequests' => 'The number of requests is more than allowed.',
+    'applicationDoesNotHaveWage' => 'The application dose not have wage.',
+    'validation' => 'The given data was invalid.',
+    'unauthenticated' => 'Unauthenticated.',
+    'unauthorized' => 'Unauthorized.',
+    'generalServerError' => 'Error occurred. Please try again.',
+];
---
+++ src/Languages/fa/exceptions.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'invalidArgument' => "پارامتر وارد شده نامعتبر است",
+    'applicationIsNotActive' => 'اپلیکیشن مربوط به درخواست شما فعال نیست',
+    'checkoutHasNotWithdrawalState' => 'این صورتحساب قابل پرداخت نیست',
+    'modelNotFound' => 'خطای ۴۰۴ رخ داده است',
+    'methodNotAllowed' => 'متد موردنظر مجاز نیست.',
+    'throttleRequests' => 'تعداد درخواست بیشتر از حد مجاز است',
+    'validation' => 'اطلاعات وارد شده نامعتبر است',
+    'unauthenticated' => 'احراز هویت انجام نشده است',
+    'unauthorized' => 'مجاز به انجام این کار نیستید',
+    'applicationDoesNotHaveWage' => 'برای اپلیکیشن مورد نظر کارمزدی ثبت نشده است',
+    'generalServerError' => 'خطایی رخ داده است، لطفا دوباره تلاش کنید',
+];
