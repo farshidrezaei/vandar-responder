138f3561c2dae1e5c4ca421e0e5727eb4c28d571
Farshid Rezaei <farshid.net1@yahoo.com>
2022-07-20 12:45:15 +0430

    add custom exceptions to config file

--- src/Configs/config.php
+++ src/Configs/config.php
@@ -4,6 +4,22 @@
  */
 
 
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomAuthenticationException;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomDefaultException;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomMethodNotAllowed;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomNotFoundHttpException;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomThrottleRequestsException;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomUnauthorizedException;
+use FarshidRezaei\VandarResponder\CustomExceptions\CustomValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+
 return [
     'errors' => [
         'UNKNOWN_ERROR' => 'unknown_error',
@@ -14,6 +30,7 @@ return [
         'UNAUTHORIZED_ERROR' => 'unauthorized_error',
         'UNAUTHENTICATED_ERROR' => 'unauthenticated_error',
         'TOO_MANY_REQUESTS_ERROR' => 'too_many_requests_error',
+        'METHOD_NOT_ALLOWED' => 'method_not_allowed',
         'GENERAL_ERROR' => 'general_error',
         'FORBIDDEN_ERROR' => 'forbidden_error',
         'CHECKOUT_CAN_NOT_BE_VERIFIED_ERROR' => 'checkout_can_not_be_verified',
@@ -24,5 +41,17 @@ return [
         'MANDATE_NOT_USABLE_ERROR' => 'mandate_not_usable',
         'WITHDRAW_TIMEOUT_ERROR' => 'withdraw_timeout',
         'INSUFFICIENT_BALANCE_ERROR' => 'insufficient_balance',
+    ],
+    'customExceptions' => [
+        RuntimeException::class => CustomDefaultException::class,
+        Exception::class => CustomDefaultException::class,
+        ValidationException::class => CustomValidationException::class,
+        NotFoundHttpException::class => CustomNotFoundHttpException::class,
+        MethodNotAllowedException::class => CustomMethodNotAllowed::class,
+        ModelNotFoundException::class => CustomNotFoundHttpException::class,
+        AuthenticationException::class => CustomAuthenticationException::class,
+        AuthorizationException::class => CustomUnauthorizedException::class,
+        UnauthorizedHttpException::class => CustomUnauthorizedException::class,
+        ThrottleRequestsException::class => CustomThrottleRequestsException::class
     ]
 ];
